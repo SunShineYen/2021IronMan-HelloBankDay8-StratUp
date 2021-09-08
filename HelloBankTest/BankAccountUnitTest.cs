d25592fe25ef5b9c716423ff9b91d82ba0938ead
SunShineYen <sunshineyen@g.ncu.edu.tw>
2021-09-08 22:23:06 +0800

    Day 8-單元測試完善 HelloBank 與基礎總結 (單元測試基礎-7) - StartUp

---
+++ HelloBankTest/BankAccountUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Bank;
+using NUnit.Framework;
+
+namespace HelloBankTest
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private BankAccount account = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            account = new BankAccount(1000);
+        }
+
+        [Test]
+        public void Adding_Funds_Updates_Balance()
+        {
+            // Act
+            account.Add(500);
+
+            // Assert
+            Assert.AreEqual(1500, account.Balance);
+        }
+
+        [Test]
+        [Category("Exception Tests")]
+        public void Adding_Negative_Funds_Throws()
+        {
+            // Act + Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Add(-500));
+        }
+
+        [Test]
+        public void Withdrawing_Funds_Updates_Balance()
+        {
+            // Act
+            account.Withdraw(500);
+
+            // Assert
+            Assert.AreEqual(500, account.Balance);
+        }
+
+        [Test]
+        [Ignore("This is an example for ignore demo")]
+        public void Transfering_Funds_Updates_Both_Account()
+        {
+            // Nothing...
+        }
+    }
+}
